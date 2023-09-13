0cd227c45ea4281240fd99acfaf19ce673f02be2
Bastien <bastien.couder@gmail.com>
2023-09-13 15:31:59 +0200

    SocialNetwork

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -20,25 +20,24 @@ router.get(
 
   async function (req, res) {
     try {
-      // Vérifiez si l'email existe déjà dans la base de données
-      const existingUser = await userModel.findOne({ email: req.user.email });
+      // // Vérifiez si l'email existe déjà dans la base de données
+      // const existingUser = await userModel.findOne({ email: req.user.email });
 
-      if (existingUser) {
-        // L'utilisateur existe déjà, connectez-le en créant un token JWT
-        const token = createToken(existingUser._id);
+      // if (existingUser) {
+      const token = createToken(existingUser._id);
 
-        res.cookie("jwt", token, {
-          httpOnly: false,
-          maxAge,
-          secure: true,
-          sameSite: "none",
-        });
-        console.log(token);
+      res.cookie("jwt", token, {
+        httpOnly: false,
+        maxAge,
+        secure: true,
+        sameSite: "none",
+      });
+      console.log(token);
 
-        res.redirect(`${process.env.CLIENT_URL}`);
-      } else {
-        res.redirect(`${process.env.CLIENT_URL}/register`);
-      }
+      res.redirect(`${process.env.CLIENT_URL}`);
+      // } else {
+      //   res.redirect(`${process.env.CLIENT_URL}/register`);
+      // }
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
